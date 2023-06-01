8a51eb395f95e9c1d183375a1a5a123a30a4ef85
Damon <126731021+damon314159@users.noreply.github.com>
2023-06-01 16:44:20 +0100

    Creates methods for event listeners and reset.
    Methods are part of the display controller module pattern. Still to do
    is the markCell internal method in display controller to tie it together
    but progress enough was made to warrant a commit before attempting that

--- script.js
+++ script.js
@@ -49,17 +49,47 @@ const displayController = (()=>{
     return node.id.slice(5);
   };
 
+  function _markCell(event) {
+    //TODO
+    //access cell with gameBoard.board[`cell_${_getIdNum(event.target)}`]
+    //access turn player with looping over i to make array players[`player${i}`], get index of true
+  }
+
+  const _addListeners = () => {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(node => node.addEventListener("click", _markCell, {once : true}))
+  };
+  //run once to initialize
+  _addListeners();
+
+  const _removeListeners = () => {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(node => node.removeEventListener("click", _markCell))
+  };
+
   //public
-  const renderDisplay = () => {
+  const renderDisplay = (clear=false) => {
     const cells = document.querySelectorAll(".cell");
     cells.forEach(node => {
-      //sets the displayed value of each cell to its stored value in the board object
+      //if passed clear, renders empty
+      if (clear=true) {
+        node.innerHTML = " ";
+      } else {
+      //else sets the displayed value of each cell to its stored value in the board object
       node.innerHTML = gameBoard.board[`cell_${_getIdNum(node)}`];
+      };
     })
+  };
+
+  const resetGame = () => {
+    _removeListeners();
+    _addListeners();
+    renderDisplay(true); //clears
   };
 
   return {
     renderDisplay,
+    resetGame,
   };
 })();
 
