de7e4ec8e5413a76bc59e05217a5fa0b976139bb
Damon <126731021+damon314159@users.noreply.github.com>
2023-06-01 06:48:28 +0100

    Add template modules and factories
    This is my first time using these patterns, so I'm committing this, even
    though it is relatively empty still

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict"
+
+
+//modules
+//-------
+
+const gameBoard = (()=>{
+  //const _privateMethod=()=>{}
+  //const publicMethod=()=>{}
+  return {
+    //publicMethod,
+  };
+})();
+
+const gameFlow = (()=>{
+  //const _privateMethod=()=>{}
+  //const publicMethod=()=>{}
+  return {
+    //publicMethod,
+  };
+})();
+
+const displayController = (()=>{
+  //const _privateMethod=()=>{}
+  //const publicMethod=()=>{}
+  return {
+    //publicMethod,
+  };
+})();
+
+
+//factories
+//---------
+
+const Player = () => {
+  //const _privateMethod=()=>{}
+  //const publicMethod=()=>{}
+  return {
+    //publicMethod,
+  };
+}
